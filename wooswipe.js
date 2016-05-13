fcc21451b221baf9279da928f71b66d037c2e780
Eric Jinks <ericjinks@gmail.com>
2016-05-13 13:57:12 +1000

    General Cleanup / Added Picturefill

--- wooswipe.js
+++ wooswipe.js
@@ -1,4 +1,4 @@
-;(function(document, window, $) {
+(function(document, window, $) {
   'use strict';
 
   (function productThumbnails() {
--- wooswipe.php
+++ wooswipe.php
@@ -158,7 +158,6 @@ function wooswipe_scripts_method() {
 
     if($options['white_theme']) wp_enqueue_style( 'white_theme', $wooswipe_wp_plugin_path . '/pswp/white-skin/skin.css'  );
     else wp_enqueue_style( 'pswp-skin', $wooswipe_wp_plugin_path . '/pswp/default-skin/default-skin.css'  );
-
     wp_enqueue_style( 'slick-css', $wooswipe_wp_plugin_path . '/slick/slick.css'  );
     wp_enqueue_style( 'slick-theme', $wooswipe_wp_plugin_path . '/slick/slick-theme.css'  );
 
@@ -166,7 +165,9 @@ function wooswipe_scripts_method() {
     wp_enqueue_script( 'pswp-ui', $wooswipe_wp_plugin_path . '/pswp/photoswipe-ui-default.min.js', null, null, true );
 
 		wp_enqueue_script( 'slick', $wooswipe_wp_plugin_path .'/slick/slick.min.js', null, null, true );
-    wp_enqueue_script( 'wooswipe_main', $wooswipe_wp_plugin_path .'/main.js', null, null, true );
+
+		wp_enqueue_script( 'picturefill', $wooswipe_wp_plugin_path .'/picturefill.min.js' ); // Load in header
+    wp_enqueue_script( 'wooswipe_main', $wooswipe_wp_plugin_path .'/wooswipe.js', null, null, true );
 }
 add_action('wp_enqueue_scripts', 'wooswipe_scripts_method');
 
@@ -182,163 +183,117 @@ function wooswipe_deregister_styles() {
 	wp_deregister_style( 'woocommerce_prettyPhoto_css' );
 }
 
-
 ///////////////////////
 //override Woo Gallery
 remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
 add_action( 'woocommerce_before_single_product_summary', 'wooswipe_woocommerce_show_product_thumbnails', 20 );
 
-
 function wooswipe_woocommerce_show_product_thumbnails(){
+	global $post, $woocommerce, $product; ?>
 
-	global $post, $woocommerce, $product;
-
-	?>
 	<div id="wooswipe" class="images">
-	<?php
 
-	if ( has_post_thumbnail() ) {
+		<?php
+		if ( has_post_thumbnail() ) {
+			$image_title = esc_attr( get_the_title( get_post_thumbnail_id() ) );
+			$image_link  = wp_get_attachment_url( get_post_thumbnail_id() );
 
-		$image_title = esc_attr( get_the_title( get_post_thumbnail_id() ) );
-		$image_link  = wp_get_attachment_url( get_post_thumbnail_id() );
+			$image_src = wp_get_attachment_image_src( get_post_thumbnail_id(), '1000w' );
+			$hq = wp_get_attachment_image_src( get_post_thumbnail_id(), '1800w' );
 
-		$image_src = wp_get_attachment_image_src( get_post_thumbnail_id(), '1000w' );
-		$hq = wp_get_attachment_image_src( get_post_thumbnail_id(), '1800w' );
+			$image = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ),
+				array(
+					'title' => '',
+					'data-hq' => $hq[0],
+					'data-w' => $hq[1],
+					'data-h' => $hq[2],
+					)
+			);
 
-		$image       = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
-			'title' => '',
-			'data-hq' => $hq[0],
-			'data-w' => $hq[1],
-			'data-h' => $hq[2],
-			) );
+			$attachment_count = count( $product->get_gallery_attachment_ids() );
 
-		$attachment_count = count( $product->get_gallery_attachment_ids() );
+			$gallery = $attachment_count > 0 ? '[product-gallery]' : '';
 
-		if ( $attachment_count > 0 ) {
-			$gallery = '[product-gallery]';
+			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '
+				<div class="single-product-main-image">
+					<a href="%s"  class="woocommerce-main-image zoom" title="%s" >%s</a>
+				</div>',
+				$image_link, $image_title, $image ), $post->ID );
 		} else {
-			$gallery = '';
+			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
 		}
 
-		echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '
-			<div class="single-product-main-image">
-				<a href="%s"  class="woocommerce-main-image zoom" title="%s" >%s</a>
+		$attachment_ids = $product->get_gallery_attachment_ids();
+		if ( $attachment_ids ) {
+			$loop 		= 0;
+			$columns 	= apply_filters( 'woocommerce_product_thumbnails_columns', 3 );
+			?>
+			<div class="thumbnails">
+					<ul class="thumbnail-nav">
+						<?php /// add main image
+							if ( has_post_thumbnail() ) {
+
+								$attachment_id 	= get_post_thumbnail_id();
+								$image       	= wp_get_attachment_image( $attachment_id, 'shop_thumbnail' );
+								$hq       		= wp_get_attachment_image_src( $attachment_id, 'full' );
+								$med       		= wp_get_attachment_image_src( $attachment_id, 'shop_single' );
+
+								echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '
+									<li>
+										<div class="thumb" data-hq="%s" data-w="%s" data-h="%s" data-med="%s" data-medw="%s" data-medh="%s">%s</div>
+									</li>',
+									$hq[0], $hq[1], $hq[2], $med[0], $med[1], $med[2], $image ), $attachment_id, $post->ID );
+
+							} //endif has_post_thumbnail
+
+							foreach ( $attachment_ids as $attachment_id ) {
+								$classes = array( '' );
+
+								if ( $loop == 0 || $loop % $columns == 0 )
+									$classes[] = '';
+								if ( ( $loop + 1 ) % $columns == 0 )
+									$classes[] = '';
+
+								$image_link = wp_get_attachment_url( $attachment_id );
+								if ( ! $image_link )
+									continue;
+
+								$image	= wp_get_attachment_image( $attachment_id, 'shop_thumbnail' );
+								$hq     = wp_get_attachment_image_src( $attachment_id, 'full' );
+								$med    = wp_get_attachment_image_src( $attachment_id, 'shop_single' );
+
+								$image_class = esc_attr( implode( '', $classes ) );
+								$image_title = '';
+								echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '
+									<li>
+										<div class="%s thumb" title="%s" data-hq="%s" data-w="%s" data-h="%s" data-med="%s" data-medw="%s" data-medh="%s">%s</div>
+									</li>'
+									, $image_class, $image_title, $hq[0], $hq[1], $hq[2], $med[0], $med[1], $med[2], $image ), $attachment_id, $post->ID, $image_class );
+
+								$loop++;
+							} //endforeach $attachment_ids ?>
+					</ul>
+
 			</div>
-			', $image_link, $image_title, $image ), $post->ID );
-
-	} else {
-
-		echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
-
-	}
-
-	$attachment_ids = $product->get_gallery_attachment_ids();
-	if ( $attachment_ids ) {
-
-		$loop 		= 0;
-		$columns 	= apply_filters( 'woocommerce_product_thumbnails_columns', 3 );
-		?>
-		<div class="thumbnails">
-
-				<ul class="thumbnail-nav">
-
-					<?php /// add main image
-						if ( has_post_thumbnail() ) {
-
-							$attachment_id 	= get_post_thumbnail_id();
-							$image       	= wp_get_attachment_image( $attachment_id, 'shop_thumbnail' );
-							$hq       		= wp_get_attachment_image_src( $attachment_id, 'full' );
-							$med       		= wp_get_attachment_image_src( $attachment_id, 'shop_single' );
-
-							echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '
-								<li>
-									<div class="thumb" data-hq="%s" data-w="%s" data-h="%s" data-med="%s" data-medw="%s" data-medh="%s">%s</div>
-								</li>'
-								, $hq[0], $hq[1], $hq[2], $med[0], $med[1], $med[2], $image ), $attachment_id, $post->ID );
-
-						}
-					?>
-
-					<?php
-						foreach ( $attachment_ids as $attachment_id ) {
-
-							$classes = array( '' );
-
-							if ( $loop == 0 || $loop % $columns == 0 )
-								$classes[] = '';
-							if ( ( $loop + 1 ) % $columns == 0 )
-								$classes[] = '';
-
-							$image_link = wp_get_attachment_url( $attachment_id );
-							if ( ! $image_link )
-								continue;
-
-							$image	= wp_get_attachment_image( $attachment_id, 'shop_thumbnail' );
-							$hq     = wp_get_attachment_image_src( $attachment_id, 'full' );
-							$med    = wp_get_attachment_image_src( $attachment_id, 'shop_single' );
-
-							$image_class = esc_attr( implode( '', $classes ) );
-							$image_title = '';
-							echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '
-								<li>
-									<div class="%s thumb" title="%s" data-hq="%s" data-w="%s" data-h="%s" data-med="%s" data-medw="%s" data-medh="%s">%s</div>
-								</li>'
-								, $image_class, $image_title, $hq[0], $hq[1], $hq[2], $med[0], $med[1], $med[2], $image ), $attachment_id, $post->ID, $image_class );
-
-							$loop++;
-						}
-					?>
-
-
-				</ul>
-
-		</div>
-		<?php
-	}
-
-	?>
+		<?php } ?>
 	</div>
-	<?php
 
-	echo'
-	<!-- Root element of PhotoSwipe. Must have class pswp. -->
+	<!-- PSWP -->
 	<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-
-	    <!-- Background of PhotoSwipe.
-	         Its a separate element as animating opacity is faster than rgba(). -->
 	    <div class="pswp__bg"></div>
-
-	    <!-- Slides wrapper with overflow:hidden. -->
 	    <div class="pswp__scroll-wrap">
-
-	        <!-- Container that holds slides.
-	            PhotoSwipe keeps only 3 of them in the DOM to save memory.
-	            Dont modify these 3 pswp__item elements, data is added later on. -->
 	        <div class="pswp__container">
 	            <div class="pswp__item"></div>
 	            <div class="pswp__item"></div>
 	            <div class="pswp__item"></div>
 	        </div>
-
-	        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
 	        <div class="pswp__ui pswp__ui--hidden">
-
 	            <div class="pswp__top-bar">
-
-	                <!--  Controls are self-explanatory. Order can be changed. -->
-
 	                <div class="pswp__counter"></div>
-
 	                <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-
 	                <button class="pswp__button pswp__button--share" title="Share"></button>
-
 	                <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-
 	                <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-
-	                <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
-	                <!-- element will get class pswp__preloader--active when preloader is running -->
 	                <div class="pswp__preloader">
 	                    <div class="pswp__preloader__icn">
 	                      <div class="pswp__preloader__cut">
@@ -347,28 +302,18 @@ function wooswipe_woocommerce_show_product_thumbnails(){
 	                    </div>
 	                </div>
 	            </div>
-
 	            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
 	                <div class="pswp__share-tooltip"></div>
 	            </div>
-
 	            <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
 	            </button>
-
 	            <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
 	            </button>
-
 	            <div class="pswp__caption">
 	                <div class="pswp__caption__center"></div>
 	            </div>
-
 	        </div>
-
 	    </div>
-
 	</div>
 
-	';
-
-
-}
+<?php } ?>
