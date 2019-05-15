28de421c545b8aca49dfb261e05b2c1f0ea02872
Pankaj <pankajs@itpathsolutions.co.in>
2019-05-15 18:25:16 +0530

    Change version of wooswipe

--- wooswipe.php
+++ wooswipe.php
@@ -6,7 +6,7 @@ Description: This is a image gallery plugin for WordPress built using <a href="h
 
 Author: Thrive Website Design
 Author URI: https://thriveweb.com.au/
-Version: 1.1.6.8
+Version: 1.1.6.9
 Text Domain: wooswipe
 */
 
