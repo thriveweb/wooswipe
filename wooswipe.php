0481481e871d7d9765027dea0f896efeccba6cc8
Dean Oakley <dean@thriveweb.com.au>
2021-03-18 15:11:03 +1000

    Update version

--- wooswipe.php
+++ wooswipe.php
@@ -6,7 +6,7 @@ Description: This is a image gallery plugin for WordPress built using <a href="h
 
 Author: Thrive Website Design
 Author URI: https://thriveweb.com.au/
-Version: 1.1.8.11
+Version: 1.1.10
 Text Domain: wooswipe
 */
 
