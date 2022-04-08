e152c0abbd4c587637c78a10cbcd857977684e82
krutimitpath <krutim@itpathsolutions.in>
2022-04-08 16:12:49 +0530

    Updated expand svg

--- admin.js
+++ admin.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function () {
   jQuery("input#icon_bg_color").change(function () {
     var bgcolor = jQuery(this).val();
     jQuery(".icons-preview-list li").each(function () {
-      jQuery(this).find('svg').children('ellipse').attr('fill', bgcolor);
       jQuery(this).find('svg').each(function () { jQuery(this).find('circle').attr('fill', bgcolor) });
 
     });
